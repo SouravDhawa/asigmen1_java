559e0c210a61e6f14bcbf82fb3c5ee0d03acd814
SouravDhawa <100849954+SouravDhawa@users.noreply.github.com>
2022-05-31 21:48:05 +0530

    Update Car.java

--- c2_exam/q3/Car.java
+++ c2_exam/q3/Car.java
@@ -1,6 +1,25 @@
 package Exam2.q3;
 
 public class Car {
-    int numberOfPassenger;
-    int numberOfKms;
+   private int numberOfPassenger;
+   private int numberOfKms;
+
+    public int getNumberOfPassenger() {
+        return numberOfPassenger;
+    }
+
+    public void setNumberOfPassenger(int numberOfPassenger) {
+        this.numberOfPassenger = numberOfPassenger;
+    }
+
+    public int getNumberOfKms() {
+        return numberOfKms;
+    }
+
+    public void setNumberOfKms(int numberOfKms) {
+        this.numberOfKms = numberOfKms;
+    }
+   public Car(){
+
+    }
 }
